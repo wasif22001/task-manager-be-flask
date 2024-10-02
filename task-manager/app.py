fb2a27774e4ed0b52d9c11e3f9d00145069cc6bf
wasif22001 <wasif22001@gmail.com>
2024-10-02 12:58:45 +0500

    Implemented CRUD operations for Tasks

--- task-manager/app.py
+++ task-manager/app.py
@@ -1,10 +1,63 @@
-from flask import Flask
+
+from flask import Flask, render_template, redirect, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-@app.route('/')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:admin@localhost:3306/task_manager'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    completed = db.Column(db.Boolean, default=False)
+
+with app.app_context():
+    db.create_all()
+
+
+@app.route('/health')
 def health():
     return 'App is up and running!'
+
+@app.route('/')
+def index():
+    tasks = Task.query.all()
+    return render_template('index.html', tasks=tasks)
+
+@app.route('/add',methods=['POST'])
+def add_task():
+    task_content = request.form['content']
+    new_task = Task(content=task_content)
+
+    db.session.add(new_task)
+    db.session.commit()
+
+    return redirect('/')
+
+@app.route('/edit/<int:task_id>', methods=['GET','POST'])
+def edit_task(task_id):
+    task = Task.query.get_or_404(task_id)
+
+    print (request)
+    print(request.method)
+
+    if request.method=='POST':
+        task.content = request.form['content']
+        db.session.commit()
+        return redirect('/')
+
+    print(request.method)
+
+    return render_template('edit.html',task=task)
+
+@app.route('/delete/<int:task_id>', methods=['POST'])
+def delete_task(task_id):
+    task=Task.query.get_or_404(task_id)
+    db.session.delete(task)
+    db.session.commit()
+    return redirect('/')
 
 
 
